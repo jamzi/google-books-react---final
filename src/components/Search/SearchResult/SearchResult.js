f37c522f502ba878a4c332e9a0e095bb6be8d8cc
Janez <janez.cadez007@gmail.com>
2018-02-10 12:31:58 +0100

    replacing http with https for thumbnail urls

--- src/components/Search/SearchResult/SearchResult.js
+++ src/components/Search/SearchResult/SearchResult.js
@@ -6,19 +6,32 @@ import genericBook from './generic-book.png';
 
 const SearchResult = (props) => {
     const { book } = props;
-    const smallThumbnail = (book.volumeInfo && book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.smallThumbnail) || genericBook;
-    const authorList = book.volumeInfo && book.volumeInfo.authors && book.volumeInfo.authors.map((author, index) => {
-        return <span key={index}>{author}</span>
-    });
+    const smallThumbnail = getThumbnail(book) || genericBook;
+    const authorList = getAuthorList(book);
+
+    function getThumbnail(book) {
+        let thumbnailUrl = book.volumeInfo && book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.smallThumbnail;
+        return thumbnailUrl ? thumbnailUrl.replace(/^http:\/\//i, 'https://') : '';
+    }
+
+    function getAuthorList(book) {
+        let authorList = book.volumeInfo && book.volumeInfo.authors && book.volumeInfo.authors;
+        if (!authorList) {
+            return <span>No authors to display</span>;
+        }
+        return authorList.map((author, index) => {
+            return <span key={index}>{author}</span>;
+        });
+    }
 
     return (
         <Link to={`book/${book.id}`}>
             <div className="book">
-                <img className="book-thumbnail" src={smallThumbnail ? smallThumbnail : ''} alt={smallThumbnail ? book.volumeInfo.title : 'no-image'} />
+                <img className="book-thumbnail" src={smallThumbnail} alt={smallThumbnail ? book.volumeInfo.title : 'no-image'} />
                 <div className="book-info">
                     <div className="book-info-title">{book.volumeInfo.title}</div>
                     <ul className="book-info-authors">
-                        {authorList ? authorList: 'No authors to display'}
+                        {authorList}
                     </ul>
                 </div>
             </div>
