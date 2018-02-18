f5f74d18d9ea32684768f28dbc23807e978b48bc
Janez <janez.cadez007@gmail.com>
2018-02-18 17:35:53 +0100

    adding photo to navbar header, login link if not userdata from firebase ls entry

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,6 +10,14 @@ import MenuIcon from 'material-ui-icons/Menu';
 import Drawer from 'material-ui/Drawer';
 import List, { ListItem, ListItemText } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
+import Avatar from 'material-ui/Avatar';
+import { withStyles } from 'material-ui/styles';
+
+const styles = {
+    flex: {
+        flex: 1,
+    }
+};
 
 class Header extends Component {
     constructor(props) {
@@ -17,11 +25,20 @@ class Header extends Component {
 
         this.state = {
             redirectToReferrer: false,
-            drawerOpened: false
+            drawerOpened: false,
+            userData: {}
         }
 
         this.onLogOut = this.onLogOut.bind(this);
         this.toggleDrawer = this.toggleDrawer.bind(this);
+    }
+
+    componentDidMount() {
+        const userKey = Object.keys(localStorage)
+            .filter(it => it.startsWith('firebase:authUser'))[0];
+        const user = userKey ? JSON.parse(localStorage.getItem(userKey)) : undefined;
+
+        this.setState({ userData: user });
     }
 
     onLogOut() {
@@ -40,15 +57,25 @@ class Header extends Component {
     }
 
     render() {
+        const { classes } = this.props;
+        const { userData } = this.state;
+
         return (
             <AppBar position="static" color="default">
                 <Toolbar>
                     <IconButton color="inherit" aria-label="Menu" onClick={this.toggleDrawer}>
                         <MenuIcon />
                     </IconButton>
-                    <Typography variant="title" color="inherit">
+                    <Typography variant="title" color="inherit" className={classes.flex}>
                         Google Books React
                     </Typography>
+                    {
+                        userData ? 
+                            <Avatar alt="Google profile image" src={userData && userData.photoURL} /> :
+                            <NavLink to="/login " style={{ textDecoration: 'none', color: 'unset' }} >
+                                <div>Login</div>
+                            </NavLink >
+                    }
                 </Toolbar>
                 <Drawer open={this.state.drawerOpened} onClose={this.toggleDrawer}>
                     <div
@@ -90,4 +117,4 @@ class Header extends Component {
     }
 }
 
-export default Header;
+export default withStyles(styles)(Header);
