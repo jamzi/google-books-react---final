79042b27d0161afdb686fe4b48e273a13cc7a573
Janez <janez.cadez007@gmail.com>
2018-02-18 17:01:16 +0100

    left drawer navigation links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import firebase from '@firebase/app';
 import '@firebase/auth';
+import { NavLink } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import Drawer from 'material-ui/Drawer';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+import Divider from 'material-ui/Divider';
 
 class Header extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            redirectToReferrer: false
+            redirectToReferrer: false,
+            drawerOpened: false
         }
 
         this.onLogOut = this.onLogOut.bind(this);
+        this.toggleDrawer = this.toggleDrawer.bind(this);
     }
 
     onLogOut() {
@@ -27,14 +35,56 @@ class Header extends Component {
             });
     }
 
+    toggleDrawer() {
+        this.setState({ drawerOpened: !this.state.drawerOpened });
+    }
+
     render() {
         return (
             <AppBar position="static" color="default">
                 <Toolbar>
+                    <IconButton color="inherit" aria-label="Menu" onClick={this.toggleDrawer}>
+                        <MenuIcon />
+                    </IconButton>
                     <Typography variant="title" color="inherit">
                         Google Books React
                     </Typography>
                 </Toolbar>
+                <Drawer open={this.state.drawerOpened} onClose={this.toggleDrawer}>
+                    <div
+                        role="button"
+                        onClick={this.toggleDrawer}
+                        onKeyDown={this.toggleDrawer}>
+                        <List>
+                            <NavLink to="/" style={{ textDecoration: 'none', color: 'unset' }} >
+                                <ListItem button>
+                                    <ListItemText primary="Home" />
+                                </ListItem>
+                            </NavLink >
+                            <NavLink to="/search" style={{ textDecoration: 'none', color: 'unset' }} >
+                                <ListItem button>
+                                    <ListItemText primary="Search" />
+                                </ListItem>
+                            </NavLink >
+                            <NavLink to="/recommended" style={{ textDecoration: 'none', color: 'unset' }} >
+                                <ListItem button>
+                                    <ListItemText primary="Recommended" />
+                                </ListItem>
+                            </NavLink >
+                            <NavLink to="/bookshelves" style={{ textDecoration: 'none', color: 'unset' }} >
+                                <ListItem button>
+                                    <ListItemText primary="Bookshelves" />
+                                </ListItem>
+                            </NavLink >
+                            <Divider />
+                            <NavLink to="/login " style={{ textDecoration: 'none', color: 'unset' }} >
+                                <ListItem button>
+                                    <ListItemText primary="Login" />
+                                </ListItem>
+                            </NavLink >
+                        </List>
+                    </div>
+                </Drawer>
             </AppBar>
         )
     }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import {
-  Link
-} from 'react-router-dom';
 
 const Home = () => (
   <div>
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/search">Search</Link></li>
-      <li><Link to="/recommended">Recommended</Link></li>
-      <li><Link to="/bookshelves">Bookshelves</Link></li>
-      <li><Link to="/login">Login</Link></li>
-    </ul>
+    <h1>Home</h1>
   </div>
 )
 
