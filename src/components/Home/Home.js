805241a9f6d0c0a72833479f53fee3f0d762b3d5
Janez <janez.cadez007@gmail.com>
2018-03-10 16:30:02 +0100

    horizontal bookshelves list on home initial implementation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,13 +5,19 @@ import Button from 'material-ui/Button';
 import ReactGA from 'react-ga';
 import { Redirect } from 'react-router-dom';
 
+import { getBooksFromBookshelf } from '../../utils/books';
+import HorizontalBooksList from '../Home/HorizontalBooksList/HorizontalBooksList';
+import './Home.css';
+
 class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       isLoggedIn: false,
-      redirectToReferrer: false
+      redirectToReferrer: false,
+      booksForYou: [],
+      booksReadingNow: []
     }
     this.handleLoginClick = this.handleLoginClick.bind(this);
     this.handleLogoutClick = this.handleLogoutClick.bind(this);
@@ -26,6 +32,27 @@ class Home extends Component {
       .listen(() => {
         this.setState({ isLoggedIn: getLoginState() })
       });
+  }
+
+  componentDidMount() {
+    if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
+      this.fetchBooksForYou();
+      this.fetchBooksReadingNow();
+    }
+  }
+
+  fetchBooksForYou() {
+    const booksForYouId = 8;
+    getBooksFromBookshelf(booksForYouId).then((books) => {
+      this.setState({ booksForYou: books });
+    });
+  }
+
+  fetchBooksReadingNow() {
+    const booksReadingNow = 3;
+    getBooksFromBookshelf(booksReadingNow).then((books) => {
+      this.setState({ booksReadingNow: books });
+    });
   }
 
   handleLoginClick() {
@@ -48,18 +75,29 @@ class Home extends Component {
   }
 
   render() {
-    const { from } = this.props.location.state || { from: { pathname: "/" }};
-    const { isLoggedIn, redirectToReferrer } = this.state;
-    let button;
+    const { from } = this.props.location.state || { from: { pathname: "/" } };
+    const { isLoggedIn, redirectToReferrer, booksForYou, booksReadingNow } = this.state;
+    let content;
 
     if (isLoggedIn) {
-      button = <Button variant="raised" color="primary" onClick={this.handleLogoutClick}>
-        Logout
-                </Button>
+      content =
+        <div>
+          <HorizontalBooksList title={'Books for you'} books={booksForYou} />
+          <HorizontalBooksList title={'Reading now'} books={booksReadingNow} />
+
+          <div class="actions">
+            <Button variant="raised" color="primary" onClick={this.handleLogoutClick}>
+              Logout
+            </Button>
+          </div>
+        </div>
     } else {
-      button = <Button variant="raised" color="primary" onClick={this.handleLoginClick}>
-        Log in with Google
-                </Button>
+      content =
+        <div class="actions">
+          <Button variant="raised" color="primary" onClick={this.handleLoginClick}>
+            Log in with Google
+          </Button>
+        </div>
     }
 
     if (redirectToReferrer) {
@@ -68,7 +106,7 @@ class Home extends Component {
 
     return (
       <div>
-        {button}
+        {content}
       </div>
     )
   }
---
+++ src/components/Home/HorizontalBooksList/HorizontalBooksList.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import GridList, { GridListTile } from 'material-ui/GridList';
+import { withStyles } from 'material-ui/styles';
+import { Link } from 'react-router-dom';
+
+import './HorizontalBooksList.css';
+
+const styles = theme => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      flexWrap: 'nowrap',
+      // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+      transform: 'translateZ(0)',
+    },
+    title: {
+      color: theme.palette.primary.light,
+    },
+    titleBar: {
+      background:
+        'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+    },
+  });
+
+const HorizontalBooksList = (props) => {
+    const {
+        title = '',
+        books = [],
+        classes
+    } = props;
+
+    return (
+        <div className="book-list">
+            <h3>{ title }</h3>
+            <GridList className={classes.gridList} cols={2.5}>
+                {books.map(book => (
+                    <GridListTile key={book.volumeInfo.id}>
+                        <Link to={`/book/${book.id}`}>
+                            {<img className="book-detail-image" src={book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.smallThumbnail} alt={book.volumeInfo.title} />}
+                        </Link>
+                    </GridListTile>
+                ))}
+            </GridList>
+        </div>
+    )
+}
+
+export default withStyles(styles)(HorizontalBooksList);
