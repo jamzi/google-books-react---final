19a95ae07694441d2ec8525f5d4b5de3351c9af9
Janez Čadež <janez.cadez007@gmail.com>
2019-01-10 22:34:47 +0100

    final fix

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 const config = {
   clientId:
-    "372206492533-lo0ruhorholbn7l5h5i8r3c68pdnelfi.apps.googleusercontent.com",
+    "372206492533-2ud5i2m8138p8btroaogjegdvrbsj3gp.apps.googleusercontent.com",
   discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/books/v1/rest"],
   scope: "https://www.googleapis.com/auth/books"
 };
