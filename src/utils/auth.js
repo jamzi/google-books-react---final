eb0e824ed07940edb1c12b20398c299018d16b2d
Janez Čadež <janez.cadez007@gmail.com>
2019-01-10 22:19:33 +0100

    new clientId, deploy fixes

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,8 @@
 const config = {
-    clientId: '727048091557-c1m6tcl1b21k6g2j61oikoa2d8caqd67.apps.googleusercontent.com',
-    discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/books/v1/rest'],
-    scope: 'https://www.googleapis.com/auth/books'
+  clientId:
+    "372206492533-lo0ruhorholbn7l5h5i8r3c68pdnelfi.apps.googleusercontent.com",
+  discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/books/v1/rest"],
+  scope: "https://www.googleapis.com/auth/books"
 };
 
 export default config;
